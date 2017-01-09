49366af268b6e40e006480a00520862925a1ae61
Kyle Wood <woodk53@gmail.com>
2017-01-09 15:55:47 -0500

    Added possible anwers mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :surveys do
-    resources :questions
+    resources :questions do
+      resources :possible_answers
+    end
   end
   root 'survey#index'
 end
